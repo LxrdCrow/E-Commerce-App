38976c6805da1e93cb23f86a3161c88664f3c707
Federico <federico.grasso606@gmail.com>
2025-03-03 17:29:04 +0100

    App with laravel

--- app/Http/Controllers/ProvaController.php
+++ app/Http/Controllers/ProvaController.php
@@ -7,5 +7,15 @@ use Illuminate\Http\Request;
 
 class ProvaController extends Controller
 {
-    //
+    public function provaFunction()
+    {
+        $calcolo = 1+1;
+
+        return $calcolo;
+    }
+
+    public function provaData(Request $request)
+    {
+        return 'Dati ricevuti: ' . $request->input('data');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProvaController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/prova', [ProvaController::class, 'provaFunction']);
+Route::post('/prova', [ProvaController::class, 'provaData']);
